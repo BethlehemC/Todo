b3e922c7a5b2f4ec3628983cfcccfb8486b95e61
BethlehemC <117508340+BethlehemC@users.noreply.github.com>
2022-11-09 23:11:03 +0300

    first commit

--- src/Login.js
+++ src/Login.js
@@ -6,30 +6,26 @@ const [email,setEmail]=useState("");
 const [pass,setPass]=useState("");
 
 
-function handelLogin(e){
-    e.preventdefault();
-console.log(email);
-console.log(pass);
-document.getElementById("login").reset();
+// function handelLogin(e){
+//     e.preventdefault();
+// console.log(email);
+// console.log(pass);
+// document.getElementById("login").reset();
+// }
 
-
-}
-
-
-
-
+const handelLogin = (e)=>{
+    // e.preventdefault();
+    console.log("email", email)}
 return (
 <div>
-
-
-    <form  onSubmit={handelLogin}  id="login">
+<form  onSubmit={handelLogin}  id="login">
 <label htmlFor="email">Email</label>
 <input type="email"
 required
 autoComplete="off"
 id="email"
 value={email}
-onChange={e=>setEmail(e.target.value)}
+onChange={(e)=>setEmail(e.target.value)}
 /><p></p>
 <label htmlFor="password">Password</label>
 <input type="password"
@@ -37,7 +33,7 @@ required
 autoComplete="off"
 id="password"
 value={pass}
-onChange={e=>setPass(e.target.value)}
+onChange={(e)=>setPass(e.target.value)}
 /><p></p>
 <button>Login</button>
 <p>{email}</p>
